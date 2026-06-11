b8b4d4c3fea1f2913cb6592f724e0e4ff33b9eda
Aleksandr Ognev <ognev.aleksandr@beresnev.games>
2026-06-11 12:23:03 +0300

    add specialized exception

---
+++ Assets/Packages/com.gamecore.analyticservice/Runtime/Exceptions/AnalyticsServiceException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GameCore.AnalyticService
+{
+    public class AnalyticsServiceException : Exception
+    {
+        public AnalyticsServiceException()
+        {
+
+        }
+
+        public AnalyticsServiceException(string message)
+            : base(message)
+        {
+
+        }
+
+        public AnalyticsServiceException(string message, Exception inner) : base(message, inner)
+        {
+
+        }
+    }
+}
